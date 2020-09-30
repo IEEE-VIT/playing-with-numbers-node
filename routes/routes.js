bd4f549b652703d6c80a688a8ed9403755d54fe8
Abhinav kumar <abhinav72610@gmail.com>
2020-10-01 00:46:40 +0530

    added palindrome logic

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,17 @@ router.post('/factorial', (req, res) => {
 
 router.post('/palindrome', (req, res) => {
     //Palindrome logic goes here
+    function palindrome(str) {
+ var re = /[^A-Za-z0-9]/g;
+ str = str.toLowerCase().replace(re, '');
+ var len = str.length;
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
+}
 })
 
 module.exports = router;
