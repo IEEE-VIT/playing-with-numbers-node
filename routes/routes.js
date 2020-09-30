98887103ff9320b6c3bcce3ef80665bdd7c6fe93
Divyansh <divyanshkhandelwal147@gmail.com>
2020-10-01 01:18:33 +0530

    added route for amicable number

--- routes/routes.js
+++ routes/routes.js
@@ -8,4 +8,32 @@ router.post('/palindrome', (req, res) => {
     //Palindrome logic goes here
 })
 
+router.post('/amicable', (req, res) => {
+    //Amicable logic goes here
+    try {
+
+        const x = req.body.x
+        const y = req.body.y
+        let sumx
+        let sumy
+        for (i = 1; i < x; i++) {
+            if (x % i == 0)
+                sumx += i
+        }
+        for (i = 1; i < y; i++) {
+            if (y % i == 0)
+                sumy += i
+        }
+
+        if (sumx == y && sumy == x) {
+            res.send({ message: "The Number is Amicable Number" }).status(200)
+        } else {
+            res.send({ message: "The Number is Not Amicable Number" }).status(200)
+        }
+
+    } catch (error) {
+        res.send({ Error: error }).status(500)
+    }
+})
+
 module.exports = router;
