e56393728d45d0f557cef379a77bf0337e4ee0da
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-10-01 03:17:47 +0530

    added /magic route to check whether a number is a magic number or not

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,22 @@
-const router = require('express')();
+const router = require("express")()
 
-router.post('/factorial', (req, res) => {
-    //Factorial logic goes here
+router.post("/factorial", (req, res) => {
+  //Factorial logic goes here
 })
 
-router.post('/palindrome', (req, res) => {
-    //Palindrome logic goes here
+router.post("/palindrome", (req, res) => {
+  //Palindrome logic goes here
 })
 
-module.exports = router;
+router.post("/magic", (req, res) => {
+  //taking the input from the body
+  const number = req.body.number
+
+  if (number % 9 == 1) {
+    return res.send(`${number} is a Magic number`)
+  } else {
+    return res.send(`${number} is not a Magic number`)
+  }
+})
+
+module.exports = router
