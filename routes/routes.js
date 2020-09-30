30aa0879f1126ae7378f2ab2bf5ac3908a2c7893
Harsh Udai <harshudai2001@gmail.com>
2020-10-01 00:21:18 +0530

    Update routes.js
    First contibution. with basic task.
    Thnak you

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,32 @@
 const router = require('express')();
 
+function prime(n){
+    let count=0;
+    for(let i=2;i<n;i++){
+        if(n%i==0){
+            count=count+1;
+        }
+    }
+    if(count>0){
+        return "Not Prime";
+    }
+    else{
+        return "Prime";
+    }
+}
+
+
 router.post('/factorial', (req, res) => {
     //Factorial logic goes here
 })
 
 router.post('/palindrome', (req, res) => {
     //Palindrome logic goes here
+})
+
+router.post('/prime', (req,res)=> {
+    var num=req.param;
+    res.send(prime(num));
 })
 
 module.exports = router;
