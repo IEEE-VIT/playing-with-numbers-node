583805c1a5542db0c2116ec176a83d9cb870ee9e
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2020-09-30 20:03:58 -0300

    add configuration to jest

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const routes = require('./routes/routes');
+const routes = require('./src/routes/routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +11,5 @@ app.use(routes);
 app.get('/', (req, res) => {
     res.send('Backend running successfully');
 })
-
-app.listen(5000, () => console.log('Server running on port 5000'));
 
 module.exports = app;
