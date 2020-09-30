01ad98bab00be7a16bd0c2e099d463da4fc8c315
Harsh Udai <harshudai2001@gmail.com>
2020-10-01 00:50:32 +0530

    Update routes.js
    First Contribution, added the prime check
    Thank you

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,31 @@
 const router = require('express')();
 
+function prime(n){
+    let count=0;
+    for(let i=2;i<n;i++){
+        if(n%i==0){
+            count=count+1;
+        }
+    }
+    if(count>0){
+        return "Not Prime";
+    }
+    else{
+        return "Prime";
+    }
+}
+
 router.post('/factorial', (req, res) => {
     //Factorial logic goes here
 })
 
 router.post('/palindrome', (req, res) => {
+    //Palindrome logic goes here
+})
+
+router.post('/prime/:num', (req, res) => {
+    var num=req.params.num;
+    res.send(prime(num));
     //Palindrome logic goes here
 })
 
