0a66e71d8e020308f50c872cd14e2bd557f4d181
nhcarrigan <nhcarrigan@gmail.com>
2020-09-30 12:33:05 -0700

    Add reverse route

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,20 @@
-const router = require('express')();
+const router = require("express")();
 
-router.post('/factorial', (req, res) => {
-    //Factorial logic goes here
-})
+router.post("/factorial", (req, res) => {
+  //Factorial logic goes here
+});
 
-router.post('/palindrome', (req, res) => {
-    //Palindrome logic goes here
-})
+router.post("/palindrome", (req, res) => {
+  //Palindrome logic goes here
+});
+
+router.post("/reverse", (req, res) => {
+  const originalString = req.body.string;
+  const reverseString = originalString.split("").reverse().join("");
+  res.json({
+    original: originalString,
+    reverse: reverseString,
+  });
+});
 
 module.exports = router;
