80f610cb4b7b347254d2f2da3538d160f2dd6517
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-10-01 16:41:15 +0530

    added /perfect number for the perfect number

--- routes/routes.js
+++ routes/routes.js
@@ -105,6 +105,24 @@ router.post('/disarium', (req, res) => {
         res.status(500).send({ Error: error })
     }
 });
+router.post("/perfect", (req, res) => {
+  try {
+    let number = req.body.number;
+    let sum = 0;
+    for (let i = 1; i <= number / 2; i++) {
+      if (number % i == 0) {
+        sum += i;
+      }
+    }
+    if (sum == number) {
+      res.status(200).send({ message: "The Number is a Perfect Number" });
+    } else {
+      res.status(200).send({ message: "The Number is Not a Perfect Number" });
+    }
+  } catch (error) {
+    res.status(500).send({ Error: error });
+  }
+});
 
 module.exports = router;
 
