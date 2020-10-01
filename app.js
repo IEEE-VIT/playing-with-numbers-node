0e380b5c7a06cbfd806ff8cedd02812549482447
prajwal-scorpionking123 <prajwal.scorpionking@gmail.com>
2020-10-01 10:13:12 +0530

    updated routes as mentioned

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const routes = require("./routes/neonRoute");
+const routes = require("./routes/routes");
 
-//path to neon route
-const neonRoute = require('./routes/neonRoute');
+
 
 
 app.use(bodyParser.json());
@@ -13,14 +12,14 @@ app.use(bodyParser.urlencoded({
 }));
 
 
-//route for checking whether number is neon or not
-//the complete route is /check/neon/:num
-app.use("/check", neonRoute);
+
+
 app.use(routes);
 
 app.get('/', (req, res) => {
     res.send('Backend running successfully');
 })
+
 
 
 //app is running here
--- routes/routes.js
+++ routes/routes.js
@@ -8,4 +8,26 @@ router.post('/palindrome', (req, res) => {
     //Palindrome logic goes here
 })
 
+
+//checking Neon Number
+router.get('/neon/:num', (req, res) => {
+    let num = Number.parseInt(req.params.num);
+    let square = num * num
+    let sum = 0;
+    while (square != 0) {
+        sum = sum + square % 10;
+        square = Math.floor(square / 10);
+    }
+
+    if (sum === num) {
+        return res.json({
+            "isNeon": true
+        })
+    } else {
+        return res.json({
+            "isNeon": false
+        })
+    }
+
+});
 module.exports = router;
