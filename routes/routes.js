8b9536cda957f5b1925fd0f367e5329129ab34c6
Avn007 <adityanadkarni007@gmail.com>
2020-10-01 12:59:52 +0530

    Added route for Armstrong Number

--- routes/routes.js
+++ routes/routes.js
@@ -8,4 +8,28 @@ router.post('/palindrome', (req, res) => {
     //Palindrome logic goes here
 })
 
+router.post('/armstrong', (req, res) => {
+    let number = req.body
+    let numberOfDigits = number.length;
+    let sum = 0;
+
+    let temp = number;
+
+    while (temp > 0) {
+
+        let remainder = temp % 10;
+
+        sum += remainder**numberOfDigits;
+
+        temp = parseInt(temp / 10);
+    }
+
+    if (sum == number) {
+        res.send({ message: "The Number is an Armstrong Number" }).status(200)
+    }
+    else {
+        res.send({ message: "The Number is not an Armstrong Number" }).status(200)
+    }
+})
+
 module.exports = router;
