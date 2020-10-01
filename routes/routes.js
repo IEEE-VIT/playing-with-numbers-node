46df7dd952c6a89654c1f52b96565de0827c3109
Amey Gupta <47378678+amey-g@users.noreply.github.com>
2020-10-01 12:21:48 +0530

    Update routers.js
    Added disarium number route to routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -37,6 +37,36 @@ router.post('/dudeney', (req, res) => {
     } catch (error) {
         res.send({ Error: error }).status(500)
     }
+	
+router.post('/disarium', (req, res) => {
+    //Disarium Number
+    try {
+	let number = req.body.number
+        let noOfDigits = number.toString().length
+
+        let sum = 0; // Initialize sum of terms 
+        let x = number; 
+         let y = noOfDigits 
+        while (y!==0) 
+        { 
+            let r = Math.floor(x%10);
+            // Sum the digits by powering according to 
+            // the positions 
+            sum = (sum + Math.pow(r, noOfDigits));
+            noOfDigits -= 1 
+            x = x/10;
+            y-- 
+        } 
+       
+        // If sum is same as number, then number is a disarium number 
+        if (sum == number) {
+            res.status(200).send({ message: "The Number is a Disarium Number" })
+        } else {
+            res.status(200).send({ message: "The Number is Not a Disarium Number" })
+        }
+    } catch (error) {
+        res.send({ Error: error }).status(500)
+    }
 
 })
 
