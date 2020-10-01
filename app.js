6543aa760c4dd4db6d7652b8f31b035c63bfd4da
prajwal-scorpionking123 <prajwal.scorpionking@gmail.com>
2020-10-01 14:28:08 +0530

    removed extra whitespace and modify the response

--- app.js
+++ app.js
@@ -3,26 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const routes = require("./routes/routes");
 
-
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
-
-
 app.use(routes);
-
 app.get('/', (req, res) => {
     res.send('Backend running successfully');
 })
-
-
-
 //app is running here
 app.listen(5000, () => console.log('Server running on port 5000'));
-
 module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -8,26 +8,30 @@ router.post('/palindrome', (req, res) => {
     //Palindrome logic goes here
 })
 
-
 //checking Neon Number
-router.get('/neon/:num', (req, res) => {
-    let num = Number.parseInt(req.params.num);
-    let square = num * num
-    let sum = 0;
-    while (square != 0) {
-        sum = sum + square % 10;
-        square = Math.floor(square / 10);
-    }
+router.post('/neon', (req, res) => {
+    try {
+        let num = Number.parseInt(req.body.num);
+        let square = num * num
+        let sum = 0;
+        while (square != 0) {
+            sum = sum + square % 10;
+            square = Math.floor(square / 10);
+        }
 
-    if (sum === num) {
-        return res.json({
-            "isNeon": true
-        })
-    } else {
-        return res.json({
-            "isNeon": false
-        })
+        if (sum === num) {
+            return res.status(200).send({
+                "Massage": "Number is Neon"
+            });
+        } else {
+            return res.status(200).send({
+                "Message": "Number is Not Neon"
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            "error": err
+        });
     }
-
 });
 module.exports = router;
