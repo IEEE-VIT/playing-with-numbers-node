5cd4652d2247ee059f0f5a3717ecb88913e522b5
Abhinav kumar <abhinav72610@gmail.com>
2020-10-01 11:43:01 +0530

    added input-output

--- routes/routes.js
+++ routes/routes.js
@@ -6,17 +6,28 @@ router.post('/factorial', (req, res) => {
 
 router.post('/palindrome', (req, res) => {
     //Palindrome logic goes here
-    function palindrome(str) {
+ try{
+
+
+    let str = req.body.str
+
  var re = /[^A-Za-z0-9]/g;
  str = str.toLowerCase().replace(re, '');
  var len = str.length;
  for (var i = 0; i < len/2; i++) {
    if (str[i] !== str[len - 1 - i]) {
-       return false;
+       res.satus(200).send({ message: "The string is not palindrome"})
+
    }
- }
- return true;
 }
+ else{
+   res.status(200).send({message:"The string is palindrome"})
+}
+ }catch(error){
+    res.send({Error:error}).status(500)
+ }
+
 })
 
 module.exports = router;
+
