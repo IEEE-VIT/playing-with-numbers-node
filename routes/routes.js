9a4ed0339ce9f5d27f083fdfc427cea215513731
Amey Gupta <47378678+amey-g@users.noreply.github.com>
2020-10-01 09:56:48 +0530

    Add factorial logic in routes.js
    added the logic for returning the factorial of the number in routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,19 @@
 const router = require('express')();
 
 router.post('/factorial', (req, res) => {
-    //Factorial logic goes here
+    //Factorial logic 
+    try {
+        let number = req.body.number
+        let factorial = 1
+        while (number !== 1) {
+            factorial = factorial * number
+            number = number - 1
+        }
+        res.status(200).send({ factorial: factorial })
+    } catch (error) {
+        res.send({ Error: error }).status(500)
+    }
+
 })
 
 router.post('/palindrome', (req, res) => {
