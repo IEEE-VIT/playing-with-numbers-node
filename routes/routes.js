9b3d6659ee46360ff45551f33ec3cf922a54c413
Ziyan Karmali <43719443+ZiyanK@users.noreply.github.com>
2020-10-03 11:11:31 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -106,22 +106,22 @@ router.post('/disarium', (req, res) => {
     }
 });
 router.post("/perfect", (req, res) => {
-  try {
-    let number = req.body.number;
-    let sum = 0;
-    for (let i = 1; i <= number / 2; i++) {
-      if (number % i == 0) {
-        sum += i;
-      }
-    }
-    if (sum == number) {
-      res.status(200).send({ message: "The Number is a Perfect Number" });
-    } else {
-      res.status(200).send({ message: "The Number is Not a Perfect Number" });
-    }
-  } catch (error) {
-    res.status(500).send({ Error: error });
-  }
+	try {
+		let number = req.body.number;
+		let sum = 0;
+		for (let i = 1; i <= number / 2; i++) {
+			if (number % i == 0) {
+				sum += i;
+			}
+		}
+		if (sum == number) {
+			res.status(200).send({ message: "The Number is a Perfect Number" });
+		} else {
+			res.status(200).send({ message: "The Number is Not a Perfect Number" });
+		}
+	} catch (error) {
+		res.status(500).send({ Error: error });
+	}
 });
 
 module.exports = router;
