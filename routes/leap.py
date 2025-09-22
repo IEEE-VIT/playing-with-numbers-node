f9a6b3200ecbe458dac2f551d501ad9fbbd3ab68
kforkandasamy <sai.shreya2025@vitstudent.ac.in>
2025-09-22 16:28:04 +0530

    Add Integer Input Statement for Year

--- routes/leap.py
+++ routes/leap.py
@@ -1,5 +1,1 @@
-yr=int(input("Enter a year to check whether it is a leap year:"))
-if yr/4==0:
-    print(yr,"is a leap year")
-else:
-    print(yr,"is not a leap year")
+yr=int(input("Enter a year to check if it is a leap year or not:"))
